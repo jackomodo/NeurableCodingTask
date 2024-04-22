468c978e12846bfdef4b87de622735e63f1d75d7
jackomodo <jacksonmai@gmail.com>
2024-04-22 09:55:31 -0500

    Initial Commit

---
+++ NeurableCodingTask/NeurableCodingTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeurableCodingTaskApp.swift
+//  NeurableCodingTask
+//
+//  Created by Jackson Mai on 4/22/24.
+//
+
+import SwiftUI
+
+@main
+struct NeurableCodingTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
